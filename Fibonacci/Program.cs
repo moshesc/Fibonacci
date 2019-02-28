3925caa1d41feda424b940a9b08340b3c5cfee8b
moshesc <beepbeep9860@gmail.com>
2019-02-28 09:58:52 +0000

    Commit

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -8,8 +8,30 @@ namespace Fibonacci
 {
     class Program
     {
+        #region Fiobonacci Method
+        //Create a static method to calculate Fibonacci numbers
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+        #endregion
+
+        #region Main
         static void Main(string[] args)
         {
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
         }
+        #endregion 
     }
 }
